f80d873dfe789d95f16f88384125447bb7585da6
Unknown <conor.proud@tessella.com>
2018-10-24 16:01:26 +0100

    Added demo file for sprint 1 and updated ftpClient python script to send a command to a gridFTP server.

---
+++ demo-scripts/sprint2/ftpClient.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+'''
+Demo script to set up a python ftp client, which
+connects to a gridftp server set up in anonymous mode using:
+globus-gridftp-server -control-interface 127.0.0.1 -aa -p 5000
+'''
+import ftplib
+from ftplib import FTP
+
+host = 'localhost'
+port = 5000
+
+ftp = FTP()
+ftp.connect(host, port)
+ftp.login()
+
+# Check that you are able to send commands (print working dir)
+print('Send command to print working dir to gridFTP server...')
+print('Server returned: ')
+print('\t' + ftp.sendcmd('PWD'))
+
+# Send a custom registered command
+#ftp.sendcmd('SITE', args)
