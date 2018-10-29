1582a27b8fffb472aa1748aef38f92bfe8a55aad
Daniel Hunt <hundt@tessella.co.uk>
2018-10-29 15:40:32 +0000

    Set-up then steps and some utilities

---
+++ systemTests/test-scenarios/then/then_steps.py
@@ -0,0 +1,18 @@
+from .log_file_steps import LogFileSteps
+from .folder_steps import FolderSteps
+
+class ThenSteps:
+	def __init__(self, context):
+		self._context = context
+
+		self.console_output = LogFileSteps(context, 'console-stdout.log')
+		
+		self.staging_folder = FolderSteps(context, 'testing_area/staging')
+
+	def simple_test_file_is_transferred(self):
+		assert 1==0
+		# should check that the file copied by "when_steps" method "simple_file_is_copied" is pasted in the correct output directory
+
+	def contents_of_copied_file_unchanged(self):
+		assert 1==0
+		#check that file copied across has the same contents as the input file
